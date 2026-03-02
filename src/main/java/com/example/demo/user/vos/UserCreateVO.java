055678e52c08b0685c2f2d2ba511af4eeb53337a
Behzod <alzosidi@gmail.com>
2026-03-02 07:50:57 +0500

    User create done

--- src/main/java/com/example/demo/user/vos/UserCreateVO.java
+++ src/main/java/com/example/demo/user/vos/UserCreateVO.java
@@ -1,8 +1,6 @@
 package com.example.demo.user.vos;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
